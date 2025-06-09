e95ecbc72a1278eff004a4096f068966fc6a6262
Beridze0 <beridzedavit904@gmail.com>
2025-06-09 23:18:27 +0400

    helper functions and configurations

--- src/js/model.js
+++ src/js/model.js
@@ -1,16 +1,14 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-    console.log(data);
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -24,7 +22,8 @@ export const loadRecipe = async function (id) {
       ingredients: recipe.ingredients,
     };
     console.log(state.recipe);
-  } catch (error) {
-    alert(error);
+  } catch (err) {
+    // Temp error handling
+    console.error(`${err} ********`);
   }
 };
